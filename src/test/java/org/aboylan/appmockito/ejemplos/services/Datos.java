a7a9d91776e743b83dab115779760f08d408d25c
Alex Boylan <aboylan@users.noreply.github.com>
2023-11-13 20:47:12 -0600

    Comprobaciones de excepciones usando when y thenThrow

--- src/test/java/org/aboylan/appmockito/ejemplos/services/Datos.java
+++ src/test/java/org/aboylan/appmockito/ejemplos/services/Datos.java
@@ -13,6 +13,12 @@ public class Datos {
             new Examen(7L, "Historia")
     );
 
+    public static final List<Examen> EXAMENES_ID_NULL = Arrays.asList(
+            new Examen(null, "Matematicas"),
+            new Examen(null, "Lenguaje"),
+            new Examen(null, "Historia")
+    );
+
     public final static List<String> PREGUNTAS = Arrays.asList("aritmetica", "integrales",
             "derivadas", "trigonometria", "geometria");
 
